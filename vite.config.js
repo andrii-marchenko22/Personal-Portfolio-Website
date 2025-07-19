2ba16a25dece6b82a5bf8ef18909a749b680547d
Andrii Marchenko <marchenko22andrey@gmail.com>
2025-07-20 02:59:15 +0300

    fised bugs

--- vite.config.js
+++ vite.config.js
@@ -3,45 +3,17 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    assetsInclude: ['**/*.ttf', '**/*.woff', '**/*.woff2'],
-    build: {
-      sourcemap: true,
-      outDir: '../dist',
-      emptyOutDir: true,
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
-        },
-      },
-    },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-    ],
-  };
-});
+export default defineConfig(({ command }) => ({
+  root: 'src',
+  base: './',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+  plugins: [
+    injectHTML(),
+    FullReload(['./src/**/**.html']),
+    SortCss({ sort: 'mobile-first' }),
+  ],
+}));
